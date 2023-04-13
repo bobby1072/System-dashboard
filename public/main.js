0cd88d955e2050619e0b1fcdb73d296e1bae500a
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2023-04-13 01:13:24 +0100

    initial commit

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const electron = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+const { app, BrowserWindow } = electron;
+let mainWindow = null;
+app.on("ready", createWindow);
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    icon: __dirname + "/../src/images/logo.jpg",
+    width: 1024,
+    height: 1024,
+    title: "Systems dashboard",
+  });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+  mainWindow.on("page-title-updated", function (e) {
+    e.preventDefault();
+  });
+}
